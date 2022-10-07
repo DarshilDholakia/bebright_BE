b6e8e13a29b3852089ce9b1c8ac936f63e8da262
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-10-07 14:30:51 +0100

    first commit

---
+++ src/main/java/com/hackathon/bebright/BebrightApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bebright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebrightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/bebright/BebrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.bebright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
