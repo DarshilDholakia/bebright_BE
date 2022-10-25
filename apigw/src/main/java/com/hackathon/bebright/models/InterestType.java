707d4410a4ace7efc40485bb69a4df1e3642adbd
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-10-25 19:49:23 +0100

    user service, need to implement delete and post interest methods

---
+++ apigw/src/main/java/com/hackathon/bebright/models/InterestType.java
@@ -0,0 +1,14 @@
+package com.hackathon.bebright.models;
+
+public enum InterestType {
+    FOOTBALL,
+    BASKETBALL,
+    TENNIS,
+    HIPHOP,
+    JAZZ,
+    MUSEUMS,
+    MARVEL,
+    GYM,
+    SKIING,
+    FASHION
+}
---
+++ clients/src/main/java/com/hackathon/bebright/clients/users/InterestType.java
@@ -0,0 +1,14 @@
+package com.hackathon.bebright.clients.users;
+
+public enum InterestType {
+    FOOTBALL,
+    BASKETBALL,
+    TENNIS,
+    HIPHOP,
+    JAZZ,
+    MUSEUMS,
+    MARVEL,
+    GYM,
+    SKIING,
+    FASHION
+}
